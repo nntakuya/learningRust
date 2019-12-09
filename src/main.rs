d482ce7ea3a9b52eaaf80efffdb1be4b70a3401d
nntakuya <uv.nkmt.yii@gmail.com>
2019-12-10 00:30:16 +0900

    数当てゲームを作成

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::io;
+
 fn main() {
-    let x = "Hello world";
-    let y = x;
-    println!("{}", x);
+    println!("Guess the number");
+
+    println!("Please input your guess.");
+
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed: {}", guess);
 }
+
